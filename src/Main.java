bc629473f9b02fe1305b48bebfb3b1df91367709
Dima Zap <zapilenovd@gmail.com>
2021-11-27 14:04:17 +0300

    initinal commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int customerAccount = 2_000_000_000;
+        int incomingPayment = 500_000_000;
+        int total= customerAccount + incomingPayment;
+
+    }
+}
